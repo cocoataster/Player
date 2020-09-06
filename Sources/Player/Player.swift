4ff645fb591ce99e886c15de227e1fa09ffc8d17
Eric Sans <eric.sans@clearscore.com>
2020-09-06 17:47:26 +0100

    Add team to players

--- Sources/Player/Player.swift
+++ Sources/Player/Player.swift
@@ -9,18 +9,29 @@ public struct Player: Codable {
     
     public var id: Int?
     public let name: String
+    public let team: Team
     public let kart: Kart
     public let description: String
     
     public init(
         id: Int?,
         name: String,
+        team: Team,
         kart: Kart,
         description: String
     ) {
         self.id = id
         self.name = name
+        self.team = team
         self.kart = kart
         self.description = description
+    }
+}
+
+public extension Player {
+    
+    enum Team: String, Codable {
+        case mario = "MARIO"
+        case bowser = "BOWSER"
     }
 }
